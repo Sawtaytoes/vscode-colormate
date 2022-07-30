9368c2ae7d57415036ff58ab6949d497abc9f24e
Kevin Ghadyani <Saturn2888@gmail.com>
2022-07-30 15:41:44 -0500

    🎨 Improved code styling in configuration.ts

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,37 +1,74 @@
 import vscode from 'vscode'
 
-export function updateConfiguration() { 
-	const configuration = vscode.workspace.getConfiguration('colorcoder')
-	
-	tokenKinds = new Set(configuration.get('tokenKinds') ?? [
-		'class.defaultLibrary',
-		'class',
-		'enum',
-		'enumMember',
-		'event',
-		'function.defaultLibrary',
-		'function',
-		'interface',
-		'macro',
-		'method',
-		'namespace',
-		'parameter',
-		'property.readonly',
-		'property',
-		'struct',
-		'type.defaultLibrary',
-		'type',
-		'variable.readonly.defaultLibrary',
-		'variable.readonly',
-		'variable',
-	])
-	ignoredLanguages = new Set(configuration.get('ignoredLanguages') ?? [])
+export const updateConfiguration = () => { 
+	const configuration = (
+		vscode
+		.workspace
+		.getConfiguration(
+			'colormate'
+		)
+	)
+
+	ignoredLanguages = (
+		new Set(
+			(
+				configuration
+				.get(
+					'ignoredLanguages'
+				)
+			)
+			?? []
+		)
+	)
+
+	tokenKinds = (
+		new Set(
+			(
+				configuration
+				.get(
+					'tokenKinds'
+				)
+			)
+			?? [
+				'class.defaultLibrary',
+				'class',
+				'enum',
+				'enumMember',
+				'event',
+				'function.defaultLibrary',
+				'function',
+				'interface',
+				'macro',
+				'method',
+				'namespace',
+				'parameter',
+				'property.readonly',
+				'property',
+				'struct',
+				'type.defaultLibrary',
+				'type',
+				'variable.readonly.defaultLibrary',
+				'variable.readonly',
+				'variable',
+			]
+		)
+	)
 }
 
-export let tokenKinds: Set<string> = new Set([])
-export let ignoredLanguages: Set<string> = new Set()
-export let colors = [
-	'#FF00FF'
-].map(color => vscode.window.createTextEditorDecorationType({ color }))
+export let ignoredLanguages: (
+	Set<
+		string
+	>
+)
 
-vscode.workspace.getConfiguration('colorcoder')
+export let tokenKinds: (
+	Set<
+		string
+	>
+)
+
+vscode
+.workspace
+.getConfiguration(
+	'colormate'
+)
