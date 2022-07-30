c17bc6b1657aed69267e12a885dd66d32dcf9312
Kevin Ghadyani <Saturn2888@gmail.com>
2022-07-30 01:56:06 -0500

    🐛 Fixed configuration bug where no tokenKinds values were defined

--- src/configuration.ts
+++ src/configuration.ts
@@ -3,25 +3,25 @@ import vscode from 'vscode'
 export function updateConfiguration() { 
 	const configuration = vscode.workspace.getConfiguration('colorcoder')
 	
-	tokenKinds = new Set(configuration.get('tokenKinds') ?? [])
+	tokenKinds = new Set(configuration.get('tokenKinds') ?? [
+		'class',
+		'decorator',
+		'enum',
+		'event',
+		'function',
+		'label',
+		'macro',
+		'method',
+		'namespace',
+		'parameter',
+		'property',
+		'type',
+		'variable',
+	])
 	ignoredLanguages = new Set(configuration.get('ignoredLanguages') ?? [])
 }
 
-export let tokenKinds: Set<string> = new Set([
-	'class',
-	'decorator',
-	'enum',
-	'event',
-	'function',
-	'label',
-	'macro',
-	'method',
-	'namespace',
-	'parameter',
-	'property',
-	'type',
-	'variable',
-])
+export let tokenKinds: Set<string> = new Set([])
 export let ignoredLanguages: Set<string> = new Set()
 export let colors = [
 	'#FF00FF'
