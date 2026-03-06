1ee1e07a400cd1b98b20e2e439aeb2041cccad4a
Kevin Ghadyani <Saturn2888@gmail.com>
2026-03-06 00:04:47 -0600

    Fixes some other issues

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -2,4 +2,12 @@ import { defineConfig } from "@vscode/test-cli"
 
 export default defineConfig({
   files: "out/**/*.test.js",
+  // launchArgs: [
+  //   "--disable-extensions",
+  // ],
+  // mocha: {
+  //   ui: "tdd",
+  //   color: true,
+  //   timeout: 60000,
+  // },
 })
